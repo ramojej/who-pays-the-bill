ef42c1a5db1aa2a43e2a7b190384b26225b31f11
Jejomar <jejomardorongon@gmail.com>
2017-08-10 17:49:22 +0800

    Finished JS1

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,1 +1,44 @@
-console.log('Works');
+(function getLoser() {
+
+	this.applicants = [];
+
+	this.init = function() {
+		this.addApplicants();
+	};
+
+
+	function generateList(input) {
+		var value = input.value;
+
+		if(this.checkValid(value)) {
+			applicants.push(value);
+			input.value = '';
+		} else {
+			alert('Something went wrong');
+		}
+	}
+
+
+	this.addApplicants = function() {
+
+		var addBtn = document.querySelector('#add_applicant');
+
+		addBtn.addEventListener('click', function() {
+
+			var input = document.querySelector('#applicant_value');
+			generateList(input);
+		});
+	};
+
+
+	this.checkValid = function(value) {
+		if(applicants.indexOf(value) < 0 &&  value != '') {
+			return true;
+		}
+
+		return false;
+	};
+
+
+	this.init();
+})();
